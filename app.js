5057129749c0de85929bdb5833f23bdfc33247d1
Gaurav Srivastav <srivastavgaurav832@gmail.com>
2024-06-13 10:11:47 +0530

    new commit

--- app.js
+++ app.js
@@ -91,81 +91,7 @@ app.use((req, res, next) => {
 
 // route matching function 
 
-const getRoutePoints = async (origin, destination) =>{
-    const response = await directionsClient.getDirections({
-      profile: 'driving',
-      waypoints: [
-        { coordinates: origin },
-        { coordinates: destination }
-      ],
-      geometries: 'polyline'
-    }).send();
-  
-    return response.body.routes[0].geometry;
-  };
-  
-  // Function to decode polyline points
-  const decodePolyline = (polyline) => {
-    return require('@mapbox/polyline').decode(polyline);
-  };
-  
-  // Function to check if two routes overlap significantly
-  const doRoutesOverlap = async (origin1, destination1, origin2, destination2) => {
-    try {
-      const points1 = await getRoutePoints(origin1, destination1);
-      const points2 = await getRoutePoints(origin2, destination2);
-  
-      // Decode polyline points
-      const decodedPoints1 = decodePolyline(points1);
-      const decodedPoints2 = decodePolyline(points2);
-  
-      // Compare the points to see if they overlap
-      const overlap = checkOverlap(decodedPoints1, decodedPoints2);
-      if(overlap===1) return 1; // for example, check if more than 50% of the points overlap
-    } catch (error) {
-      console.error('Error comparing routes:', error);
-      return false;
-    }
-  };
-  
-  // Function to check overlap between two sets of points
-  const checkOverlap = (points1, points2) => {
-    let overlapCount = 0;
-    points1.forEach(point1 => {
-      points2.forEach(point2 => {
-        if (geolib.isPointWithinRadius(
-          { latitude: point1[0], longitude: point1[1] },
-          { latitude: point2[0], longitude: point2[1] },
-          50 // 50 meters tolerance
-        )) {
-          overlapCount++;
-        }
-      });
-    });
-    return overlapCount / points1.length;
-  };
-  
-  // Example usage
-//   const origin1 = [77.5946, 12.9716]; // Coordinates for Bangalore
-//   const destination1 = [77.6288, 12.9345]; // Coordinates for another point in Bangalore
-//   const origin2 = [77.5946, 12.9716]; // Coordinates for Bangalore
-//   const destination2 = [77.6288, 12.9345]; // Coordinates for another point in Bangalore
-  
-  doRoutesOverlap(origin1, destination1, origin2, destination2)
-    .then(overlap => {
-      if (overlap) {
-        console.log('The routes overlap significantly.');
-      } else {
-        console.log('The routes do not overlap significantly.');
-      }
-    });
-  
-    const getRider = (users,userStart,userEnd) => {
-      Object.values(users).forEach(value =>{
-          console.log(value);
-      });
-    };
-  
+
 
 
 
@@ -207,18 +133,13 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-
-function f(){
- console.log("HIII");
-}
-
 let users = {};
 
 io.on('connection', (socket) => {
     console.log('New client connected');
     let currentUserId = null;
 
-    socket.on('locationUpdate', (message) => {
+    socket.on('locationUpdate', (message) => { // for rider
         const data = message;
         const userId = data.userId;
         if (userId) {
@@ -226,12 +147,7 @@ io.on('connection', (socket) => {
         }
         const rider = data.rider;
         const timestamp = data.timestamp;
-        if (rider === 0) {
-            const start = data.start;
-            const end = data.end;
-            console.log(`Client ${userId} sent request from Start: ${start} to End: ${end} at ${timestamp}`);
-            //f();
-        } else {
+        
             const latitude = data.latitude;
             const longitude = data.longitude;
             const start = data.start;
@@ -241,11 +157,10 @@ io.on('connection', (socket) => {
                 currentUserId = userId;
                 console.log(`Rider ${userId} location updated to start: ${start}, end: ${end} at ${timestamp}`);
             }
-        }
         console.log(users);
         // console.log(message);
     });
-    socket.on('rideRequest', (message) => {
+    socket.on('rideRequest', (message) => { // for user
         const data = message;
         const userId = data.userId;
         if (userId) {
@@ -271,11 +186,6 @@ io.on('connection', (socket) => {
         clearInterval(interval);
         console.log('Client disconnected');
     });
-});
-
-module.exports = users;
-server.listen(3000, () => {
-    console.log("SERVING ON PORT 3000...");
 });
 
 // gaurav
